fd0ea6b5d06f20247dd79e1d5d09ffb8f04ed540
BrianBell422 <brianbell.uk@gmail.com>
2021-01-27 20:19:07 +0000

    Initial Commit

---
+++ modules/calculator.py
@@ -0,0 +1,14 @@
+def add(num1, num_2):
+    return num1 + num_2
+
+
+def subtract(num1, num_2):
+    return num1 - num_2
+
+
+def divide(num1, num_2):
+    return num1 / num_2
+
+
+def multiply(num1, num_2):
+    return num1 * num_2
